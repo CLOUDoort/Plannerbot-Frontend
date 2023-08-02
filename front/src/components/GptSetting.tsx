d84c3daefd3d0f5879aaddc261af09573bc95247
JunSeok <ppu1234@naver.com>
2023-08-02 21:06:38 +0900

    Update submit button

--- front/src/components/GptSetting.tsx
+++ front/src/components/GptSetting.tsx
@@ -15,10 +15,14 @@ const GptSetting = ({ setKeyword, keyword }: Props) => {
 
     const [word, setWord] = useState("")
     let idx = 3
-    const addKeyword = (item: string) => {
+    const submitWord = (e: any) => {
+        e.preventDefault()
+        addKeyword()
+    }
+    const addKeyword = () => {
         if (word) {
             setKeyword([
-                ...keyword, { item: item, idx: idx++ }
+                ...keyword, { item: word, idx: idx++ }
             ])
         }
         setWord("")
@@ -27,11 +31,11 @@ const GptSetting = ({ setKeyword, keyword }: Props) => {
     return (
         <div className='w-full'>
             <label htmlFor="require">추가 요구사항</label>
-            <form className='flex items-center justify-center w-full gap-1'>
+            <form onSubmit={submitWord} className='flex items-center justify-center w-full gap-1'>
                 <input type="text" id='require' className='w-full p-2 my-2 border rounded' placeholder='요구사항 입력' value={word} onChange={handelChange} />
-                <button onClick={() => addKeyword(word)} className='z-[10] px-4 h-10 whitespace-nowrap text-xs transition-colors bg-orange-300 rounded hover:bg-orange-400 active:bg-orange-500'>입력</button>
+                <button className='z-[10] px-4 h-10 whitespace-nowrap text-xs transition-colors bg-orange-300 rounded hover:bg-orange-400 active:bg-orange-500'>입력</button>
             </form>
-            <button className='z-[10] px-4 w-full my-2 h-10 whitespace-nowrap text-lg transition-colors bg-orange-300 rounded hover:bg-orange-400 active:bg-orange-500'>계획</button>
+            <button type='button' className='z-[10] px-4 w-full my-2 h-10 whitespace-nowrap text-lg transition-colors bg-orange-300 rounded hover:bg-orange-400 active:bg-orange-500'>계획</button>
         </div>
 
     )
