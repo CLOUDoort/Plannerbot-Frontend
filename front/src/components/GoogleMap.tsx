867bf45fed9696986e06dd7d5be11381586789e1
JunSeok <ppu1234@naver.com>
2023-08-03 23:02:34 +0900

    Add google map

---
+++ front/src/components/GoogleMap.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useMemo, useRef } from 'react'
+
+import { Loader } from "@googlemaps/js-api-loader";
+
+type Props = {
+    address: string
+}
+
+const GoogleMap = ({ address }: Props) => {
+    const mapRef = useRef<HTMLDivElement>(null);
+    const geocoder = useMemo(() => new google.maps.Geocoder(), []);
+    useEffect(() => {
+        const loader = new Loader({
+            apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+            version: "weekly",
+        });
+        loader.load().then(() => {
+            geocoder.geocode({ address: address }, (results, status) => {
+                if (results) {
+                    if (status === "OK") {
+                        const map = new google.maps.Map(mapRef.current as HTMLDivElement, {
+                            center: results[0].geometry.location,
+                            zoom: 14,
+                        });
+                        const marker = new google.maps.Marker({
+                            map: map,
+                            position: results[0].geometry.location,
+                        });
+                    } else {
+                        console.error(`Geocode was not successful for the following reason: ${status}`);
+                    }
+                } else return
+            });
+        });
+    }, [address, geocoder]);
+    return <div className='min-h-[30rem] border border-gray-400 p-2 pb-4 rounded' ref={mapRef} />;
+}
+
+
+export default GoogleMap
--- front/src/components/GptText.tsx
+++ front/src/components/GptText.tsx
@@ -1,15 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
+import GoogleMap from './GoogleMap'
+import Link from 'next/link'
 import LoadingSpinner from './LoadingSpinner'
+import { useAtomValue } from 'jotai'
+import { viewText } from '@/store/initialState'
 
-type Props = {}
+type Props = {
+}
 
 const GptText = (props: Props) => {
-    const [text, setText] = useState([])
+    const gptText = useAtomValue(viewText)
+    const [textAddress, setTextAddress] = useState("")
+    const clickMap = (text: string) => setTextAddress(text)
+
     return (
         <>
-            {!text ? <div className='w-full h-full'>
-            </div> : <LoadingSpinner />}
+            {viewText ?
+                <div className='w-full h-full overflow-y-scroll'>
+                    {Object.keys(gptText).map((keys: any) => (
+                        <div key={keys} className="py-2 my-2 border-b-2 border-black/40">
+                            <div className='py-3 text-2xl font-semibold'>{keys}</div>
+                            <div className='flex gap-1 my-2'>
+                                {gptText[keys].map((item: string) => (
+                                    <div onClick={() => clickMap(item)} className={`px-2 py-1 transition-colors border rounded cursor-pointer whitespace-nowrap bg-black/10 ${item === textAddress && "bg-red-400"} hover:bg-black/30`} key={item}>
+                                        {item}
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                    <div className='py-3 text-2xl font-semibold'>Google Map</div>
+                    {textAddress && <GoogleMap address={textAddress} />}
+                </div>
+                : <LoadingSpinner />}
         </>
     )
 }
