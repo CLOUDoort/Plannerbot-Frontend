c24951a720961e07c0be4496dda8c959fb0f9b3b
JunSeok <ppu1234@naver.com>
2023-08-05 21:53:07 +0900

    Add select

--- front/src/components/GptContainer.tsx
+++ front/src/components/GptContainer.tsx
@@ -10,10 +10,10 @@ type Props = {
 }
 
 const GptContainer = ({ place, period }: Props) => {
-    const [keyword, setKeyword] = useState([{ item: place, idx: 0 }, { item: period, idx: 1 }])
-    const deleteKeyword = (idx: number) => {
-        if (idx < 2) {
-            toast.error("기본 사항은 삭제가 불가능합니다.")
+    const [keyword, setKeyword] = useState([{ item: "장소: " + place, idx: 0, select: true }, { item: "기간: " + period, idx: 1, select: true }])
+    const deleteKeyword = (idx: number, select: boolean) => {
+        if (select) {
+            toast.error("이미 반영한 항목은 삭제가 불가능합니다.")
             return
         }
         const filtered = keyword.filter((item) => item.idx != idx)
@@ -29,10 +29,10 @@ const GptContainer = ({ place, period }: Props) => {
             <article className='flex z-[9999] flex-col items-center justify-between h-full px-4 border min-w-[15rem] shadow-2xl bg-white/80'>
                 <div className='w-full'>
                     <div className='w-full mt-6 text-2xl font-semibold text-center'>입력 키워드</div>
-                    <div className='flex flex-col mt-[2rem] gap-2 justify-center'>
+                    <div className='flex flex-col mt-[2rem] gap-2 justify-center overflow-y-scroll'>
                         {keyword.map((item) => {
                             return (
-                                <span key={item.idx} onClick={() => deleteKeyword(item.idx)} className={`p-2 overflow-hidden ${item.idx > 1 && "hover:bg-red-400"} bg-white border rounded cursor-pointer whitespace-nowrap`}>{item.item}</span>
+                                <span key={item.idx} onClick={() => deleteKeyword(item.idx, item.select)} className={`p-2 overflow-hidden ${item.idx > 1 && !item.select && "hover:bg-red-400"} bg-white border rounded cursor-pointer whitespace-nowrap`}>{item.item}</span>
                             )
                         })}
                     </div>
